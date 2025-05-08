78f5f2f28fad05695c7d5485d9e8917521253690
Macelo <maceloaugusto066@gmail.com>
2025-05-07 22:18:59 -0300

    alguns ajustes

--- public/scripts/index.controller.js
+++ public/scripts/index.controller.js
@@ -25,19 +25,19 @@ angular
     });
 
     $scope.cart = [];
-    
+
     $scope.products = [];
     $scope.cartOpenedOnce = false;
     $scope.cartTotalQuantity = 0; // ← contador total de itens no carrinho
     $scope.cartTotalPrice = 0; // ← total somado dos preços
-    
+
     $scope.getProducts = async () => {
       const response = await $http.get("http://localhost:3000/products");
       console.log(response.data);
       $scope.products = response.data;
       $scope.$apply();
     };
-    
+
     $scope.getProducts();
 
     $scope.toggleCart = function () {
@@ -66,7 +66,7 @@ angular
       $scope.cart = JSON.parse(savedCart);
       $scope.updateCartSummary();
     }
-  
+
     if (savedCart) {
       $scope.cart = JSON.parse(savedCart);
       $scope.updateCartSummary();
@@ -77,7 +77,7 @@ angular
       if (existing) {
         existing.quantity++;
       } else {
-        $scope.cart.push({...product, quantity: 1});
+        $scope.cart.push({ ...product, quantity: 1 });
       }
 
       if (!$scope.cartOpenedOnce) {
@@ -113,7 +113,7 @@ angular
     // login
 
     $scope.logged = localStorage.getItem("token") ? true : false;
-    $scope.login = {email: "", password: ""};
+    $scope.login = { email: "", password: "" };
 
     $scope.verifyLogin = async () => {
       if (!$scope.login.email || !$scope.login.password) {
@@ -140,136 +140,117 @@ angular
 
     // cadastro
 
-    $scope.cadastro = {username: "", email: "", password: "", cPassword: ""};
+    $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
 
-  $scope.logged = localStorage.getItem('token') ? true : false;
-  $scope.login = { email: "", password: "" };
+    $scope.logged = localStorage.getItem('token') ? true : false;
+    $scope.login = { email: "", password: "" };
 
-  $scope.verifyLogin = async () => {
-    if (!$scope.login.email || !$scope.login.password) {
-      console.log($scope.log.email, $scope.login.password)
-      return
-    }
-
-    try {
-      const resp = await $http.post('http://localhost:3000/auth/login', $scope.login);
-      localStorage.setItem('token', resp.data.token);
-      $window.location.href = '/'
-    } catch (error) {
-      console.log(error.data.message)
-    }
-  };
-
-  $scope.loggout = () => {
-    localStorage.removeItem('token');
-    $window.location.href = '/'
-  }
-
-
-  // cadastro
-
-  $scope.messageAlert = false;
-
-  $scope.showAlertUserC = async function (message) {
-    $scope.messageCadastro = message;
-    $scope.messageAlert = true;
-    $scope.$apply();
-
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
-    $scope.$apply(() => {
-      $scope.messageAlert = false;
-    });
-
-  }
-
-  $scope.closeMessage = (variableMessage) => { variableMessage = false; };
-
-  $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
-
-  $scope.insertUser = async () => {
-    if ($scope.cadastro.password !== $scope.cadastro.cPassword) { await $scope.showAlertUserC('Senhas estão diferentes!'); return }
-
-    try {
-      $scope.cadastro = { username: $scope.cadastro.username, email: $scope.cadastro.email, password: $scope.cadastro.password }
-      const resp = await $http.post('http://localhost:3000/singup/cadastrar', $scope.cadastro)
-      await $scope.showAlertUserC(resp.data.message);
-      $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
-    } catch (error) {
-      $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
-      console.log(error.data.message)
-      await $scope.showAlertUserC(error.data.message)
-    }
-  };
-
-  // cadastro de produtos
-
-
-  $scope.proAlert = false;
-  $scope.campsProduct = { name: '', price: '', description: '' }
-
-  $scope.showAlertUserP = async function (message) {
-    $scope.messageP = message;
-    $scope.proAlert = true;
-    $scope.$apply();
-
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
-    $scope.$apply(() => {
-      $scope.proAlert = false;
-    });
-  }
-
-  $scope.insertProduct = async () => {
-
-    if (!$scope.logged) { await $scope.showAlertUserP('Favor realizar login!') };
-
-    const inputFile = document.getElementById('imagem');
-    const file = inputFile.files[0];
-
-    try {
-      const formData = new FormData();
-      console.log(toString($scope.campsProduct.price))
-      formData.append('name', $scope.campsProduct.name);
-      formData.append('price', $scope.campsProduct.price);
-      formData.append('description', $scope.campsProduct.description);
-      formData.append('image', file);
-
-      const config = { transformRequest: angular.identity, headers: { 'Content-Type': undefined, 'Authorization': localStorage.getItem('token') } };
-
-      const response = await $http.post('http://localhost:3000/products', formData, config)
-      await $scope.showAlertUserP(response.data.message);
-      $scope.campsProduct = { name: '', price: '', description: '' }
-      inputFile.value = '';
-      $scope.getProducts();
-      $window.location.href = '/'
-    } catch (err) {
-      console.log(err.data.message);
-      await $scope.showAlertUserP(err.data.message);
-      $scope.campsProduct = { name: '', price: '', description: '' }
-      inputFile.value = '';
-      $window.location.href = '/'
-    }
-  }
-
-    // front-end functions
-});
+    $scope.verifyLogin = async () => {
+      if (!$scope.login.email || !$scope.login.password) {
+        console.log($scope.log.email, $scope.login.password)
+        return
+      }
 
       try {
-        $scope.cadastro = {
-          username: $scope.cadastro.username,
-          email: $scope.cadastro.email,
-          password: $scope.cadastro.password,
-        };
-        const resp = await $http.post(
-          "http://localhost:3000/singup/cadastrar",
-          $scope.cadastro
-        );
-        console.log(resp.data.message);
+        const resp = await $http.post('http://localhost:3000/auth/login', $scope.login);
+        localStorage.setItem('token', resp.data.token);
+        $window.location.href = '/'
       } catch (error) {
-        console.log(error.data.message);
+        console.log(error.data.message)
       }
     };
+
+    $scope.loggout = () => {
+      localStorage.removeItem('token');
+      $window.location.href = '/'
+    }
+
+
+    // cadastro
+
+    $scope.messageAlert = false;
+
+    $scope.showAlertUserC = async function (message) {
+      $scope.messageCadastro = message;
+      $scope.messageAlert = true;
+      $scope.$apply();
+
+      await new Promise(resolve => setTimeout(resolve, 5000));
+
+      $scope.$apply(() => {
+        $scope.messageAlert = false;
+      });
+
+    }
+
+    $scope.closeMessage = (variableMessage) => { variableMessage = false; };
+
+    $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
+
+    $scope.insertUser = async () => {
+      if ($scope.cadastro.password !== $scope.cadastro.cPassword) { await $scope.showAlertUserC('Senhas estão diferentes!'); return }
+
+      try {
+        $scope.cadastro = { username: $scope.cadastro.username, email: $scope.cadastro.email, password: $scope.cadastro.password }
+        const resp = await $http.post('http://localhost:3000/singup/cadastrar', $scope.cadastro)
+        await $scope.showAlertUserC(resp.data.message);
+        $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
+      } catch (error) {
+        $scope.cadastro = { username: "", email: "", password: "", cPassword: "" };
+        console.log(error.data.message)
+        await $scope.showAlertUserC(error.data.message)
+      }
+    };
+
+    // cadastro de produtos
+
+
+    $scope.proAlert = false;
+    $scope.campsProduct = { name: '', price: '', description: '' }
+
+    $scope.showAlertUserP = async function (message) {
+      $scope.messageP = message;
+      $scope.proAlert = true;
+      $scope.$apply();
+
+      await new Promise(resolve => setTimeout(resolve, 5000));
+
+      $scope.$apply(() => {
+        $scope.proAlert = false;
+      });
+    }
+
+    $scope.insertProduct = async () => {
+
+      if (!$scope.logged) { await $scope.showAlertUserP('Favor realizar login!') };
+
+      const inputFile = document.getElementById('imagem');
+      const file = inputFile.files[0];
+
+      try {
+        const formData = new FormData();
+        console.log(toString($scope.campsProduct.price))
+        formData.append('name', $scope.campsProduct.name);
+        formData.append('price', $scope.campsProduct.price);
+        formData.append('description', $scope.campsProduct.description);
+        formData.append('image', file);
+
+        const config = { transformRequest: angular.identity, headers: { 'Content-Type': undefined, 'Authorization': localStorage.getItem('token') } };
+
+        const response = await $http.post('http://localhost:3000/products', formData, config)
+        await $scope.showAlertUserP(response.data.message);
+        $scope.campsProduct = { name: '', price: '', description: '' }
+        inputFile.value = '';
+        $scope.getProducts();
+        $window.location.href = '/'
+      } catch (err) {
+        console.log(err.data.message);
+        await $scope.showAlertUserP(err.data.message);
+        $scope.campsProduct = { name: '', price: '', description: '' }
+        inputFile.value = '';
+        $window.location.href = '/'
+      }
+    }
 
     // front-end functions
   });
